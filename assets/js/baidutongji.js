d5b045590c15bfd36f867a8681386d87e97a03b8
guomengtao <rinuo@qq.com>
2025-09-11 09:48:43 +0800

    baidutongji

---
+++ assets/js/baidutongji.js
@@ -0,0 +1,8 @@
+// baidutongji.js - 百度统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0f20824c45da2773d9b6c0e33845228b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
--- main.js
+++ main.js
@@ -3,3 +3,4 @@ import '/assets/js/bookmarks.js';       // 导入即触发自启动
 import '/assets/js/email-subscription.js'; // 导入即触发自启动
 import '/assets/js/main-search.js';      // 搜索功能模块导入
 import '/assets/js/tawkService.js';      //tawkService.js 
+import '/assets/js/baidutongji.js';     // 百度统计
